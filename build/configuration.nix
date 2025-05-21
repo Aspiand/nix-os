2bca07f7d1319ccd1fa583d85afa9c7837f898f5
Aspian <muhammad.aspian.d@gmail.com>
2025-05-21 20:42:06 +0800

    build: postBuildCommands & crossSystem

--- build/configuration.nix
+++ build/configuration.nix
@@ -1,9 +1,21 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   system.stateVersion = "24.11";
-  nixpkgs.config.allowUnfree = true;
+  services.openssh.enable = true;
+  i18n.defaultLocale = "en_US.UTF-8";
   networking.hostName = "nixos-s905x";
+
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  nixpkgs = {
+    config.allowUnfree = true;
+    localSystem.system = "x86_64-linux";
+    crossSystem.system = "aarch64-linux";
+  };
 
   users.users.yuki = {
     isNormalUser = true;
@@ -16,17 +28,13 @@
     ];
   };
 
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
-
   boot = {
     kernelPackages = pkgs.linuxPackages_latest;
+    consoleLogLevel = lib.mkDefault 7;
     loader = {
       grub.enable = false;
       generic-extlinux-compatible.enable = true;
-      # generic-extlinux-compatible.configurationLimit = 1;
+      generic-extlinux-compatible.configurationLimit = 3;
     };
   };
 
@@ -35,8 +43,12 @@
     name = "meson-gxl-s905x-p212.dtb";
   };
 
-  sdImage.compressImage = false;
-  services.openssh.enable = true;
-
-  i18n.defaultLocale = "en_US.UTF-8";
+  sdImage = {
+    compressImage = false;
+    postBuildCommands = ''
+      # https://github.com/NixOS/nixpkgs/blob/master/pkgs/misc/uboot/default.nix#L290
+      dd if=${pkgs.ubootLibreTechCC}/u-boot.gxl.sd.bin of=$img conv=fsync,notrunc bs=512 seek=1 skip=1
+      dd if=${pkgs.ubootLibreTechCC}/u-boot.gxl.sd.bin of=$img conv=fsync,notrunc bs=1 count=444
+    '';
+  };
 }
